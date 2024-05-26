f403c63c89eb1641097ad6f405bfe827c6f3e0a9
shreyas <shreyas@192.168.0.101>
2024-05-26 21:58:55 +0530

    update backend code

--- apps/backends/with-postgresql-sequelize/src/server.ts
+++ apps/backends/with-postgresql-sequelize/src/server.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 
-const app = express()
+const app = express();
 
 // Define a route handler for the root path
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!")
-})
+  res.send("<h1 style='text-align: center; margin-top: 50px;'>Hello, World!</h1>");
+});
 
 // Start the server
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`🚀 Server is up and running at http://localhost:${PORT}`);
+});
